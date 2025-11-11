73cf36401120cb120e9e4fa61ddee495d9486815
jaimergp <jaimergp@users.noreply.github.com>
2025-11-11 19:52:51 +0100

    Fix filenames

--- recipe/build_sdkroot_env.sh
+++ recipe/build_sdkroot_env.sh
@@ -3,10 +3,10 @@
 find "${RECIPE_DIR}" -name "activate-sdkroot-env.sh" -exec cp {} . \;
 find "${RECIPE_DIR}" -name "deactivate-sdkroot-env.sh" -exec cp {} . \;
 
-find . -name "activate-sdkroot-env.sh" -exec sed -i.bak "s|@MACOSX_DEPLOYMENT_TARGET@|${_MACOSX_DEPLOYMENT_TARGET_}|g" "{}" \;
-find . -name "activate-sdkroot-env.sh" -exec sed -i.bak "s|@PLATFORM@|${cross_target_platform//-/_}|g" "{}" \;
-find . -name "activate-sdkroot-env.sh.bak" -exec rm "{}" \;
+find . -name "activate-sdkroot_env.sh" -exec sed -i.bak "s|@MACOSX_DEPLOYMENT_TARGET@|${_MACOSX_DEPLOYMENT_TARGET_}|g" "{}" \;
+find . -name "activate-sdkroot_env.sh" -exec sed -i.bak "s|@PLATFORM@|${cross_target_platform//-/_}|g" "{}" \;
+find . -name "activate-sdkroot_env.sh.bak" -exec rm "{}" \;
 
 mkdir -p "${PREFIX}"/etc/conda/{de,}activate.d/
-cp "${SRC_DIR}"/activate-sdkroot-env.sh "${PREFIX}"/etc/conda/activate.d/activate_"${PKG_NAME}".sh
-cp "${SRC_DIR}"/deactivate-sdkroot-env.sh "${PREFIX}"/etc/conda/deactivate.d/deactivate_"${PKG_NAME}".sh
+cp "${SRC_DIR}"/activate-sdkroot_env.sh "${PREFIX}"/etc/conda/activate.d/activate_"${PKG_NAME}".sh
+cp "${SRC_DIR}"/deactivate-sdkroot_env.sh "${PREFIX}"/etc/conda/deactivate.d/deactivate_"${PKG_NAME}".sh
