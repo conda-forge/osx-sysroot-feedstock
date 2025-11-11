4a988a07eea9ab1f4194c2ca492cb523346896f5
Isuru Fernando <isuruf@gmail.com>
2025-11-11 11:41:56 -0800

    Modify activate-sdkroot_env.sh for cross-compilation
    Updated SDKROOT environment activation script to handle cross-compilation on non-macOS platforms.

--- recipe/activate-sdkroot_env.sh
+++ recipe/activate-sdkroot_env.sh
@@ -1,17 +1,17 @@
-if [ "$(uname)" != "Darwin" ]; then
-    echo "sdkroot-env_@PLATFORM@ is only available on macOS" >&2
-    exit 1
-fi
-
 if [ "${SDKROOT:-0}" != "0" ] && [ -d "${SDKROOT:-0}" ]; then
     exit 0
 fi
 
-export _CONDA_SDKROOT_ENV_SET=1
-
 if [ "${CONDA_BUILD_SYSROOT:-0}" != "0" ] && [ -d "${CONDA_BUILD_SYSROOT:-0}" ]; then
+    export _CONDA_SDKROOT_ENV_SET=1
     export SDKROOT="${CONDA_BUILD_SYSROOT}"
     exit 0
 fi
 
+if [ "$(uname)" != "Darwin" ]; then
+    echo "Need to set SDKROOT when cross-compiling" >&2
+    exit 1
+fi
+
+export _CONDA_SDKROOT_ENV_SET=1
 export SDKROOT="$(xcrun --show-sdk-path)"
