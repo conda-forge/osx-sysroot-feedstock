a20190f845973807c911d9ddd770c41da6c1d55f
Isuru Fernando <isuruf@gmail.com>
2025-11-24 20:46:26 -0800

    use return instead of exit

--- recipe/activate-sdkroot_env.sh
+++ recipe/activate-sdkroot_env.sh
@@ -1,16 +1,16 @@
 if [ "${SDKROOT:-0}" != "0" ] && [ -d "${SDKROOT:-0}" ]; then
-    exit 0
+    return 0
 fi
 
 if [ "${CONDA_BUILD_SYSROOT:-0}" != "0" ] && [ -d "${CONDA_BUILD_SYSROOT:-0}" ]; then
     export _CONDA_SDKROOT_ENV_SET=1
     export SDKROOT="${CONDA_BUILD_SYSROOT}"
-    exit 0
+    return 0
 fi
 
 if [ "$(uname)" != "Darwin" ]; then
     echo "Need to set SDKROOT when cross-compiling" >&2
-    exit 1
+    return 1
 fi
 
 export _CONDA_SDKROOT_ENV_SET=1
